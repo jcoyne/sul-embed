da42903a237faa53ed644d490f50022272d79203
Jessie Keck <jessie.keck@gmail.com>
2019-07-02 11:37:50 -0700

    Try a scrollTo polyfill to see if it helps scrollIntoView behavior in Edge

--- app/javascript/packs/m3.js
+++ app/javascript/packs/m3.js
@@ -1,9 +1,11 @@
 import '@babel/polyfill';
 import 'url-polyfill/url-polyfill';
 import 'unfetch/polyfill';
+import smoothscroll from 'smoothscroll-polyfill';
 import { CssInjection } from '../../assets/javascripts/modules/css_injection.js';
 import { CommonViewerBehavior } from '../../assets/javascripts/modules/common_viewer_behavior.js';
 import M3Viewer from '../../assets/javascripts/modules/m3_viewer.js';
 
+smoothscroll.polyfill();
 CssInjection.appendToHead();
 CommonViewerBehavior.initializeViewer(M3Viewer.init);
